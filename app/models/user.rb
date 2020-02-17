0ccdf12b9f0d50df192b895a6a47da817a8b5e79
BearCub29 <poulichet.florian@gmail.com>
2020-02-17 16:48:22 +0100

    third version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :events, through: :attendance
   has_many :events
+  after_create :welcome_send
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ require 'faker'
 
 #User.destroy_all
 
-10.times do
+05.times do
   User.create!(
     first_name: Faker::Name.first_name, 
     last_name: Faker::Name.last_name,
