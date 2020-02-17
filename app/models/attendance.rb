4892fa4d5bff7cc8caadd90d8018a8a9250b7f73
BearCub29 <poulichet.florian@gmail.com>
2020-02-17 17:40:47 +0100

    4 version

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,10 @@
 class Attendance < ApplicationRecord
   belongs_to :user
-  belongs_to :event
+  belongs_to :event 
+
+  after_create :congrat_send
+
+  def congrat_send
+    AttendanceMailer.congrat_email(self).deliver_now
+  end
 end
