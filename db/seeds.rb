790e1a281878630a5ef5faf02b105f7e7e07bf34
BearCub29 <poulichet.florian@gmail.com>
2020-02-17 16:05:01 +0100

    second version

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+#User.destroy_all
+
+10.times do
+  User.create!(
+    first_name: Faker::Name.first_name, 
+    last_name: Faker::Name.last_name,
+    description: Faker::Lorem.sentence(word_count: rand(5..20)),
+    email: Faker::Internet.email(domain: 'yopmail.com'),
+    encrypted_password: Faker::Internet.password(min_length: 8) 
+    )
+end
+
+puts "check"
