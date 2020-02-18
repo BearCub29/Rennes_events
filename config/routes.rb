64bef35724d8438b506232d2157f8e2587de6df5
BearCub29 <poulichet.florian@gmail.com>
2020-02-18 14:42:56 +0100

    7version

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/', to: 'events_controller#index'
   devise_for :users
+  resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
