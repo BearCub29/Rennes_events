bda6fb4ede78822e815456e7895a4dc1e2274a08
BearCub29 <poulichet.florian@gmail.com>
2020-02-18 17:53:33 +0100

    9 version

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,11 +9,10 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(start_date: params[:start_date], duration: params[:duration],description: params[:description],title: params[:title],price: params[:price],location: params[:location],user_id: current_user.id)    # avec xxx qui sont les données obtenues à partir du formulaire
       if @event.save # essaie de sauvegarder en base @gossip
-        redirect_to root_path# si ça marche, il redirige vers la page d'index du site
+        redirect_to events_path# si ça marche, il redirige vers la page d'index du site
       else
         render new_event_path# sinon, il render la view new (qui est celle sur laquelle on est déjà)
       end
-  
   end
 
   def show
@@ -27,7 +26,7 @@ class EventsController < ApplicationController
   def authenticate_user
     unless current_user
       flash[:danger] = "Please log in."
-      redirect_to user_session_path
+      redirect_to user_session_path 
     end
   end
 end
