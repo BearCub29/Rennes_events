53ca776c505861e403499043c714cfcad3d9ffc4
BearCub29 <poulichet.florian@gmail.com>
2020-02-19 18:42:53 +0100

    15 versions

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,37 +4,35 @@ class AttendancesController < ApplicationController
 
   end
   def new
-    @amount = Event.find(params[:event_id]).price
+    @amount = (Event.find(params[:event_id]).price)*100
   end
+  
+
   def create
     # Amount in cents
     @amount = Event.find(params[:event_id]).price
-    
-
     customer = Stripe::Customer.create({
-      email: params[:stripeEmail],
-      source: params[:stripeToken],
+    email: params[:stripeEmail],
+    source: params[:stripeToken],
     })
-
     charge = Stripe::Charge.create({
-      customer: customer.id,
-      amount: @amount,
-      description: 'Rails Stripe customer',
-      currency: 'usd',
+    customer: customer.id,
+    amount: @amount,
+    description: 'Rails Stripe customer',
+    currency: 'usd',
     })
+    
 
-    rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
+    @attendance = Attendance.new(event_id: params[:event_id], user_id: current_user.id, strip_customer_id: params[:stripeToken])  
+    if @attendance.save 
+      flash[:ok] = "Vous êtes bien isncrit à l'évenement!!!"
+      redirect_to event_path(params[:event_id])# si ça marche, il redirige vers la page d'index du site
+    else
+      render new_event_attendance_path(params[:event_id])
+    end  
+
   end
-  def show
-    
-  end
-  def edit
-    
-  end
-  def update
-      
-  end
-  
+
+
+
 end
